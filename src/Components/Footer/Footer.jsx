d5f47338807381fdfc94e0d2c26f91a244de7eb1
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-03-25 14:30:35 -0700

    done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,12 +3,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className='footer'>
-        <div><p>Designed and developed by &copy; </p> <a href="https://www.linkedin.com/in/waleed0260/" style={{textDecoration: "none", color: "rgb(160, 158, 158)"}}><b>Muhammad Waleed Ahsan</b></a> </div>
-      <div>
-        <span>Terms and condition</span>
-        <span>Privacy Policy</span>
-        <span>Login</span>
-      </div>
+        <div><p>Designed and developed by  </p> <a href="https://www.linkedin.com/in/waleed0260/" style={{textDecoration: "none", color: "black"}}><b> &copy; Muhammad Waleed Ahsan</b></a> </div>
     </div>
   )
 }
--- src/Loader.jsx
+++ src/Loader.jsx
@@ -4,7 +4,7 @@ import loading from "./loading.gif"
 
 const Loader = () => {
   return (
-    <div style={{width: "100vw", height: "48.2vw", backgroundColor: "black", display: "flex", justifyContent: "center", alignItems:"center", flexDirection: "column"}}>
+    <div style={{width: "100vw", height: "40rem", backgroundColor: "black", display: "flex", justifyContent: "center", alignItems:"center", flexDirection: "column"}}>
       <img src={loading} alt="" />
       <h2 style={{color: "white"}}>Loading...</h2>
     </div>
