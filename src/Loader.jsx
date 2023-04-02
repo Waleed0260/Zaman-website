475a2ef6399fc98e4f99e2009bb2d82c4e8bacb7
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-04-02 15:26:21 -0700

    commit

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import loading from "./loading.gif"
+import loader from "./loader.gif"
 
 
 const Loader = () => {
   return (
     <div style={{width: "100vw", height: "40rem", backgroundColor: "black", display: "flex", justifyContent: "center", alignItems:"center", flexDirection: "column"}}>
-      <img src={loading} alt="" />
-      <h2 style={{color: "white"}}>Loading...</h2>
+      <img src={loader} alt="" />
     </div>
   )
 }
