47145dc9c9721d5d89705b8648b536efacedc63c
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-04-03 15:01:27 -0700

    done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import "./Contact.css";
 import {CiMobile4} from "react-icons/ci"
 import {AiOutlineMail} from "react-icons/ai"
 import {GrLocation} from "react-icons/gr"
-
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const [name, setName] = useState("")
+  const[email, setEmail] = useState("")
+  const[done, setDone] = useState(false)
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_970a0ul', 'template_udx4x7c', form.current, 'PQtzxGdy62bOdEKE6')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true)
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
+
+
   return (
     <div className="about-me about-contact">
       <div className="contact-me">
@@ -39,15 +62,18 @@ const Contact = () => {
             </div>
         </div>
         <div className="form">
-          <form id="usrform">
-            <input type="text" name="name" id="" placeholder="Name" />
-            <input type="email" name="email" id="" placeholder="Email"  />
+          <form ref={form} id="usrform" onSubmit={sendEmail}>
+            <input type="text" name="from_name" id="" value={name} onChange={(e)=> setName(e.target.value)} placeholder="Name" required/>
+            <input type="email" name="from_email" id="" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Email" required />
             <input type="email" name="number" id="" placeholder="Phone Number (optional)"  />
 
           </form>
-          <textarea name="comment" form="usrform" placeholder="Enter Your Message Here"></textarea>
-          <button type="submit"><b>Submit</b></button>
+          <textarea name="message" form="usrform" placeholder="Enter Your Comments Here" required></textarea>
+          <button type="submit" form="usrform"><b>Submit</b></button>
+          {done && <div style={{marginTop: "10px"}}>Thank you for contacting me</div>}
+
         </div>
+
       </div>
     </div>
   );
