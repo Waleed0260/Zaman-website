7b694abc1cafc50c56064a7bd2c19f38cb962325
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-04-11 13:46:30 -0700

    commit

--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import Heading from '../Heading/Heading'
 import "./Pages.css"
 import {AiFillFile, AiFillContacts} from "react-icons/ai"
@@ -13,15 +13,60 @@ import Blog from '../Blog/Blog'
 import Testimonial from '../Testimonial/Testimonial'
 import Service from '../Service/Service'
 
-const Pages = () => {
-  const[about, setAbout] = useState(false);
-  const[resume, setResume] = useState(false);
-  const[contact, setContact] = useState(false);
-  const[blog, setBlog] = useState(false);
-  const[test, setTest] = useState(false);
-  const[serv, setServ] = useState(false)
+const initialState = {
+  about: false,
+  resume: false,
+  contact: false,
+  blog: false,
+  test: false,
+  serv: false,
+}
 
-  // Counter states
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INITIAL":
+      return {
+        ...initialState
+      }
+    case "ABOUT":
+      return {
+        ...initialState,
+        about: true,
+      };
+    case "RESUME":
+      return {
+        ...initialState,
+        resume: true,
+      };
+    case "CONTACT":
+      return {
+        ...initialState,
+        contact: true,
+      };
+    case "BLOG":
+      return {
+        ...initialState,
+        blog: true,
+      };
+    case "TEST":
+      return {
+        ...initialState,
+        test: true,
+      };
+    case "SERV":
+      return {
+        ...initialState,
+        serv: true,
+      };
+    default:
+      return state;
+  }
+};
+
+
+
+const Pages = () => {
+
   const[clickCount, setClickCount] = useState(0)
   const[click, setClicked] = useState(0)
   const[countResume, setCountResume] = useState(0);
@@ -29,137 +74,96 @@ const Pages = () => {
   const[countTest, setCountTest] = useState(0)
   const[countBlog, setCountBlog] = useState(0);
 
+const [state, dispatch] = useReducer(reducer, initialState);
 
+const handleClick = () => {
+  setClickCount(prevCount => prevCount + 1);
+  if (clickCount % 2 === 0){
+  dispatch({ type: "ABOUT" });
+  }
+  else{
+    dispatch({type: "INITIAL"})
+  }
+};
 
-  const handleClick = () => {
-    setClickCount(prevCount => prevCount + 1);
-    if (clickCount % 2 === 0) {
-      setAbout(true);
-      setResume(false)
-      setContact(false);
-      setBlog(false);
-      setTest(false)
-      setServ(false)
-    }
-      else{
-        setAbout(false)
-      }
-  };
+const handleResume = () => {
+  setClicked(prevCount => prevCount + 1);
+  if (click % 2 === 0){
+  dispatch({ type: "RESUME" });
+  } 
+  else{
+    dispatch({type: "INITIAL"})
+  }
+};
 
+const handleServ = () => {
+  setCountResume(prevCount => prevCount + 1);
+  if (countResume % 2 === 0){
+  dispatch({ type: "SERV" });
+  }
+  else{
+    dispatch({type: "INITIAL"})
 
+  }
+};
 
-  const handleResume = () => {
-    setCountResume(prevCount => prevCount + 1);
-    if (countResume % 2 === 0) {
+const handleTest = () => {
+  setCountServ(prevCount => prevCount + 1);
+  if (countServ % 2 === 0){
+  dispatch({ type: "TEST" });
+  }
+  else{
+    dispatch({type: "INITIAL"})
+  }
+};
 
-    setAbout(false);
-    setResume(true)
-    setContact(false);
-    setAbout(false);
-    setBlog(false);
-    setTest(false)
-    setServ(false)
-    }
-    else{
-      setResume(false)
-    }
-  };
+const handleBlog = () => {
+  setCountBlog(prevCount => prevCount + 1);
+  if (countBlog % 2 === 0){
+  dispatch({ type: "BLOG" });
+  }
+  else{
+    dispatch({type: "INITIAL"})
+  }
+};
 
-  const handleServ = () => {
-    setCountServ(prevCount => prevCount + 1);
-    if (countServ % 2 === 0) {
-    setAbout(false);
-    setResume(false)
-    setContact(false);
-    setAbout(false);
-    setBlog(false);
-    setTest(false)
-    setServ(true)
-    }
-    else{
-
-      setServ(false)
-    }
-  };
-
-  const handleTest = () => {
-    setCountTest(prevCount => prevCount + 1);
-    if (countTest % 2 === 0) {
-    setAbout(false);
-    setResume(false)
-    setContact(false);
-    setAbout(false);
-    setBlog(false);
-    setTest(true)
-    setServ(false)
-    }
-    else{
-      setTest(false);
-    }
-  };
-
-  const handleBlog = () => {
-    setCountBlog(prevCount => prevCount + 1);
-    if (countBlog % 2 === 0) {
-    setAbout(false);
-    setResume(false)
-    setContact(false);
-    setAbout(false);
-    setBlog(true);
-    setTest(false)
-    setServ(false)
-    }
-    else{
-      setBlog(false)
-    }
-  };
-
-  const handleContact = () => {
-    setClicked(prevCount => prevCount + 1);
-
-    if (click % 2 === 0) {
-
-    setAbout(false);
-    setResume(false)
-    setContact(true);
-    setAbout(false);
-    setBlog(false);
-    setTest(false)
-    setServ(false)
-    }
-    else{
-      setContact(false)
-    }
-  };
-
+const handleContact = () => {
+  setCountTest(prevCount => prevCount + 1);
+  if (countTest % 2 === 0){
+  dispatch({ type: "CONTACT" });
+  } 
+  else{
+    dispatch({type: "INITIAL"})
+  }
+};
 
 
   return (
     <div>
       <Heading icons={<AiFillFile/>} text="About me" click={handleClick}/>
       <div className="about-page">
-      {about && <About/>}
+      {state.about && <About/>}
       </div>
       <Heading icons={<AiFillContacts/>} text="Resume" click={handleResume}/>
       <div className="about-page">
-      {resume && <Resume/>}
+      {state.resume && <Resume/>}
       </div>     
        <Heading icons={<TfiBag/>} text="Portfolio" />
       <Heading icons={<FiSettings/>} text="Service" click={handleServ}/>
       <div className="about-page">
-      {serv && <Service/>}
+      {state.serv && <Service/>}
       </div>
       <Heading icons={<BiMessageCheck/>} text="Testimonial" click={handleTest}/>
       <div className="about-page">
-      {test && <Testimonial/>}
+      {state.test && <Testimonial/>}
       </div>          
       <Heading icons={<FaPager/>} text="Blog" click={handleBlog}/>
       <div className="about-page">
-      {blog && <Blog/>}
+      {state.blog && <Blog/>}
       </div>          
       <Heading icons={<BiMessageRounded/>} text="Contact" click={handleContact}/>
       <div className="about-page">
-      {contact && <Contact/>}
+      {state.contact && <Contact/>}
       </div>     
 
     </div>
