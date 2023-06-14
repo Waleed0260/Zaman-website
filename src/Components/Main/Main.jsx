4e875c1c7e4f8d77753daad3fbf5ecbc3ed9c2aa
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-06-14 04:25:39 -0700

    changes

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -9,10 +9,7 @@ import {BsYoutube, BsCircleFill, BsCircle} from "react-icons/bs"
 import {RxCross1} from "react-icons/rx"
 import {BsQuora} from "react-icons/bs"
 import MyResume from "../../images/MyResume.pdf"
-
-  const myStyles = {
-    backgroundImage: "linear-gradient(to right, #feda75, #fa7e1e, #d62976, #962fbf, #4f5bd5)",
-  };
+import { PopupButton } from 'react-calendly'
 
 const Main = () => {
   return (
@@ -42,7 +39,6 @@ const Main = () => {
             <h1>Zaman Zahoor</h1>
             <p>
             Hi I am Zaman An Innovative digital marketing coordinator | Google and Facebook ads media buyer
-
             </p>
             <div className="Main-icons">
                 <span><a href="https://www.facebook.com/profile.php?id=10008944707" target="blank"><FiFacebook/></a> </span>
@@ -51,8 +47,20 @@ const Main = () => {
                 <span><a href="https://www.linkedin.com/in/zamanzahoor" target="blank"><BsLinkedin/></a></span>
                 <span><a href="https://www.youtube.com/channel/UC5jBVHi1vNjD8UKhKIq3ijQ" target="blank"><BsYoutube/></a></span>
                 <span><a href="https://www.quora.com/profile/Zaman-Zahoor-2" target="blank"><BsQuora   style={{color: "red"}}/></a></span>
-
             </div>
+            {/* <button className='concul'> </button> */}
+            <div id="root"></div>
+            
+            <PopupButton
+        url="https://calendly.com/zamanzahoor/free-consultation-to-boost-your-business?month=2023-06"
+        /*
+         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+         */
+        rootElement={document.getElementById("root")}
+        text="Book free Consultation now"
+        className='concul'
+      />
         </div>
         <div className="Main-right">
             <span></span>
