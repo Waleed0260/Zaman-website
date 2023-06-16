bd935f329a2336f35516425fd432b53e91cf87b7
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-06-16 12:45:21 -0700

    Update Testimonial.jsx

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -14,15 +14,15 @@ const Testimonial = () => {
   const clients = [
     {
       img: blog1,
-      heading: "CEO of FELIX HAWK",
-      href: "https://www.linkedin.com/company/felix-hawk/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BWGSVVeWSQwe7s6QCvxrnsw%3D%3D",
+      heading: "REACT ADMIN-PANEL",
+      href: "https://adminpanel-c7t.pages.dev/",
       review:
-        "We are incredibly fortunate to have Zaman Zahoor as our social media manager and content creator. Their creative vision and strategic approach have transformed our online presence, resulting in a significant increase in engagement and brand awareness. Their ability to consistently deliver high-quality content and effectively manage our social media channels has exceeded our expectations. Zaman Zahoor is an invaluable asset to our team, and we highly recommend their expertise to any company looking to elevate their social media strategy. [Felix-Hawk]",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto eligendi reiciendis incidunt, eos nam aut architecto quasi voluptates facilis nesciunt dolor qui in quibusdam enim optio amet illo modi consequatur, provident placeat a! Ullam, nisi. Quisquam adipisci nulla quaerat sed.",
     },
     {
       img: aboutimg,
-      heading: "Product Marketing Manager at google",
-      href: "https://www.linkedin.com/in/cindypregita/",
+      heading: "REACT RECIPE-APP",
+      href: "https://waleed0260.github.io/my-recipe-app/",
       review:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur eveniet in dicta veniam optio expedita nostrum obcaecati similique quas architecto voluptates, voluptate sequi sapiente quod fugiat veritatis ipsam possimus voluptatibus.",
     },
@@ -31,14 +31,14 @@ const Testimonial = () => {
       heading: "REACT ADMIN-PANEL",
       href: "https://adminpanel-c7t.pages.dev/",
       review:
-        "I am incredibly proud of Zaman Zahoor is growth and accomplishments as a digital marketing learner. Their dedication, enthusiasm, and commitment to mastering the various aspects of digital marketing have been truly impressive. Throughout the course, Zaman Zahoor consistently exceeded expectations, tackling every task with a high level of professionalism and creativity. Their ability to grasp complex concepts and apply them effectively in real-world scenarios is commendable. I have no doubt that Zaman Zahoor has a bright future ahead in the field of digital marketing, and I wholeheartedly endorse their skills and expertise." - [CINDY].",
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur eveniet in dicta veniam optio expedita nostrum obcaecati similique quas architecto voluptates, voluptate sequi sapiente quod fugiat veritatis ipsam possimus voluptatibus.",
     },
     {
       img: aboutimg,
-      heading: "CEO of ELO",
-      href: "https://www.exportleftovers.com/",
+      heading: "REACT RECIPE-APP",
+      href: "https://waleed0260.github.io/my-recipe-app/",
       review:
-"Working with Zaman Zahoor as our Google Ads expert has been a game-changer for our e-commerce business. Their deep knowledge and strategic approach to Google Ads have significantly increased our online visibility, driving a substantial boost in traffic and conversions. Their expertise and dedication have proven invaluable, and we highly recommend Zaman Zahoor to any company looking to achieve remarkable results with Google Ads." - [CEO of ELO],
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur eveniet in dicta veniam optio expedita nostrum obcaecati similique quas architecto voluptates, voluptate sequi sapiente quod fugiat veritatis ipsam possimus voluptatibus.",
     },
   ];
   return (
