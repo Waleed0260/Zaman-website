d9c64a4f474b988df8ad0be3b27cc7332b42c351
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-06-17 03:17:41 +0500

    Update Testimonial.jsx

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -16,17 +16,17 @@ const Testimonial = () => {
   const clients = [
     {
       img: felix,
-      heading: "REACT ADMIN-PANEL",
-      href: "https://adminpanel-c7t.pages.dev/",
+      heading: "CEO , Felixhawk",
+      href: "linkedin.com/company/felix-hawk/================================================================================================================================================================================================================================",
       review:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto eligendi reiciendis incidunt, eos nam aut architecto quasi voluptates facilis nesciunt dolor qui in quibusdam enim optio amet illo modi consequatur, provident placeat a! Ullam, nisi. Quisquam adipisci nulla quaerat sed.",
+        "We are incredibly fortunate to have Zaman Zahoor as our social media manager and content creator. Their creative vision and strategic approach have transformed our online presence, resulting in a significant increase in engagement and brand awareness. Their ability to consistently deliver high-quality content and effectively manage our social media channels has exceeded our expectations. Zaman Zahoor is an invaluable asset to our team, and we highly recommend their expertise to any company looking to elevate their social media strategy.- CEO , Felixhawk.",
     },
     {
       img: link,
-      heading: "REACT RECIPE-APP",
-      href: "https://waleed0260.github.io/my-recipe-app/",
+      heading: "CINDY",
+      href: "https://www.linkedin.com/in/cindypregita/",
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur eveniet in dicta veniam optio expedita nostrum obcaecati similique quas architecto voluptates, voluptate sequi sapiente quod fugiat veritatis ipsam possimus voluptatibus.",
+        "I am incredibly proud of Zaman Zahoor is growth and accomplishments as a digital marketing learner. Their dedication, enthusiasm, and commitment to mastering the various aspects of digital marketing have been truly impressive. Throughout the course, Zaman Zahoor consistently exceeded expectations, tackling every task with a high level of professionalism and creativity. Their ability to grasp complex concepts and apply them effectively in real-world scenarios is commendable. I have no doubt that Zaman Zahoor has a bright future ahead in the field of digital marketing, and I wholeheartedly endorse their skills and expertise. - [CINDY]",
     },
     {
       img: elo,
@@ -35,13 +35,7 @@ const Testimonial = () => {
       review:
         "Working with Zaman Zahoor as our Google Ads expert has been a game-changer for our e-commerce business. They significantly increased our online visibility, driving a substantial boost in traffic and conversions. Their expertise and dedication have proven invaluable, and we highly recommend Zaman Zahoor to any company looking to achieve remarkable results with Google Ads. -CEO of ELO",
     },
-    {
-      img: felix,
-      heading: "CINDY",
-      href: "https://waleed0260.github.io/my-recipe-app/",
-      review:
-        "I am incredibly proud of Zaman Zahoor is growth and accomplishments as a digital marketing learner. Their dedication, enthusiasm, and commitment to mastering the various aspects of digital marketing have been truly impressive. Throughout the course, Zaman Zahoor consistently exceeded expectations, tackling every task with a high level of professionalism and creativity. Their ability to grasp complex concepts and apply them effectively in real-world scenarios is commendable. I have no doubt that Zaman Zahoor has a bright future ahead in the field of digital marketing, and I wholeheartedly endorse their skills and expertise. - [CINDY]",
-    },
+   
   ];
   return (
     <div className='about-me about-test'>
